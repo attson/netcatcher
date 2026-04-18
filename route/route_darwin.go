7e017f0fdcf0bdba60cd0e994c7391e80f0b75d6
attson <attson1@outlook.com>
2026-04-18 18:10:54 +0800

    feat: prompt for admin privileges on macOS when not running as root

--- route/route_darwin.go
+++ route/route_darwin.go
@@ -1,37 +1,47 @@
 package route
 
 import (
+	"fmt"
 	"log"
 	"net"
+	"os"
 	"os/exec"
 )
 
-func AddRoute(ip, gateway string, mask net.IPMask) error {
-	var command *exec.Cmd
+func isRoot() bool {
+	return os.Geteuid() == 0
+}
 
-	if mask != nil {
-		command = exec.Command("route", "add", "-net", ip, gateway)
-	} else {
-		command = exec.Command("route", "add", "-host", ip, gateway)
+func runRouteCmd(args string) error {
+	if isRoot() {
+		command := exec.Command("sh", "-c", args)
+		command.Stderr = log.Writer()
+		command.Stdout = log.Writer()
+		return command.Run()
 	}
-
+	script := fmt.Sprintf(`do shell script "%s" with administrator privileges`, args)
+	command := exec.Command("osascript", "-e", script)
 	command.Stderr = log.Writer()
 	command.Stdout = log.Writer()
-
 	return command.Run()
 }
 
-func DeleteRoute(ip, gateway string, mask net.IPMask) error {
-	var command *exec.Cmd
-
+func AddRoute(ip, gateway string, mask net.IPMask) error {
+	var args string
 	if mask != nil {
-		command = exec.Command("route", "delete", "-net", ip, gateway)
+		args = fmt.Sprintf("route add -net %s %s", ip, gateway)
 	} else {
-		command = exec.Command("route", "delete", "-host", ip, gateway)
+		args = fmt.Sprintf("route add -host %s %s", ip, gateway)
 	}
+	return runRouteCmd(args)
+}
 
-	command.Stderr = log.Writer()
-	command.Stdout = log.Writer()
-
-	return command.Run()
+func DeleteRoute(ip, gateway string, mask net.IPMask) error {
+	var args string
+	if mask != nil {
+		args = fmt.Sprintf("route delete -net %s %s", ip, gateway)
+	} else {
+		args = fmt.Sprintf("route delete -host %s %s", ip, gateway)
+	}
+	return runRouteCmd(args)
 }
